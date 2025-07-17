3852bd9ec6bfdfb2a559e6f85b342a98381ebbdb
Swann Latreche <slatrech@f2r8s19.clusters.42paris.fr>
2025-07-17 11:53:02 +0200

    fix: fixed few things to match the behavior of the real export func

--- includes/built_ins.h
+++ includes/built_ins.h
@@ -12,6 +12,7 @@
 int     ft_pwd(t_env **env);
 int     ft_echo(char **str, t_env **env);
 int     ft_export(t_env **env, char *arg);
+int     export_loop(t_env **env, char **arg);
 int     is_exportable(char *arg);
 int     ft_cd(char **path, t_env **env);
 int     ft_unset(t_env **env, char *arg);
