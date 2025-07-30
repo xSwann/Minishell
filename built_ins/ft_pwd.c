f511a8f14216c145eeef72e08dbcbe483a34fa19
Flavien Lebrun <flebrun@f6r12s3.clusters.42paris.fr>
2025-07-30 14:53:14 +0200

    removed duplicate lines

--- built_ins/ft_pwd.c
+++ built_ins/ft_pwd.c
@@ -1,20 +1,16 @@
 #include "../includes/built_ins.h"
 
 char	*get_pwd(void)
-char	*get_pwd(void)
 {
 	char	buffer[4097];
 	char	*cwd;
 
 	cwd = getcwd(buffer, 4097);
 	if (cwd == NULL)
-	cwd = getcwd(buffer, 4097);
-	if (cwd == NULL)
 		return (NULL);
 	return (ft_strdup(cwd));
 }
 
-int	ft_pwd(t_env **env)
 int	ft_pwd(t_env **env)
 {
 	char	*pwd;
