3d8408e2cfc4a1f8443f1aa58e2baff6d2d6c2db
Swann Latreche <slatrech@f2r8s19.clusters.42paris.fr>
2025-07-31 10:51:58 +0200

    fix: normed all the parsing functions, just need the command creator

---
+++ parsing/sanitize/sanitize_tokens.c
@@ -0,0 +1,61 @@
+#include "../../includes/parsing.h"
+#include "../../includes/built_ins.h"
+
+void	handle_quote_start(t_tab *tokens, int i, int *j, char *quote)
+{
+	*quote = tokens[i].str[(*j)++];
+}
+
+void	handle_quote_end(int *j, char *quote)
+{
+	*quote = '\0';
+	(*j)++;
+}
+
+void	process_token_char(t_token_data *data)
+{
+	if (should_expand(data->tokens, data->i, *(data->j), *(data->quote)))
+	{
+		handle_expansion(data->tokens, data->env, data->i, *(data->quote));
+		*(data->j) = 0;
+		return ;
+	}
+	if (*(data->quote) == '\0'
+		&& is_quote(data->tokens[data->i].str[*(data->j)]))
+		handle_quote_start(data->tokens, data->i, data->j, data->quote);
+	else if (*(data->quote) != '\0'
+		&& data->tokens[data->i].str[*(data->j)] == *(data->quote))
+		handle_quote_end(data->j, data->quote);
+	else
+		(*(data->j))++;
+}
+
+void	sanitize_single_token(t_tab *tokens, t_env *env, int i)
+{
+	int				j;
+	char			quote;
+	t_token_data	data;
+
+	j = 0;
+	quote = '\0';
+	data.tokens = tokens;
+	data.env = env;
+	data.i = i;
+	data.j = &j;
+	data.quote = &quote;
+	while (tokens[i].str[j])
+		process_token_char(&data);
+	strip_quotes(&tokens[i].str);
+}
+
+void	sanitize_tokens(t_tab *tokens, t_env *env)
+{
+	int	i;
+
+	i = 0;
+	while (tokens[i].str)
+	{
+		sanitize_single_token(tokens, env, i);
+		i++;
+	}
+}
---
+++ parsing/sanitize/sanitize_tokens_utils3.c
@@ -0,0 +1,60 @@
+#include "../../includes/parsing.h"
+#include "../../includes/built_ins.h"
+
+void	handle_special_cases(char **str, char **to_expand, int start)
+{
+	if ((*str)[start + 1] == '?')
+		*to_expand = ft_strdup("EXIT_CODE");
+	else if (ft_isdigit((*str)[start + 1]) || is_quote((*str)[start + 1]))
+		*to_expand = ft_strdup("9");
+}
+
+void	set_to_expand(char **to_expand, char **str)
+{
+	int	i;
+	int	start;
+
+	i = 0;
+	find_dollar_position(str, &i);
+	start = i++;
+	find_variable_end(str, &i);
+	if ((*str)[start + 1] == '?' || ft_isdigit((*str)[start + 1])
+		|| is_quote((*str)[start + 1]))
+		handle_special_cases(str, to_expand, start);
+	else
+		*to_expand = ft_substr(*str, start + 1, i - (start + 1));
+}
+
+void	expand_var(char **str, t_env *env)
+{
+	int		len;
+	char	*expanded;
+	char	*to_expand;
+
+	len = 0;
+	set_to_expand(&to_expand, str);
+	expanded = get_env(env, to_expand);
+	free(to_expand);
+	if (!expanded)
+		erase_expand(str, ft_strlen(*str) - len);
+	else
+		replace_expanded_value(str, expanded, ft_strlen(*str) - len);
+	free(expanded);
+}
+
+int	should_expand(t_tab *tokens, int i, int j, char quote)
+{
+	return (tokens[i].str[j] == '$'
+		&& (quote == '\0' || quote == '"')
+		&& tokens[i].str[j + 1]
+		&& (ft_isalnum(tokens[i].str[j + 1])
+			|| tokens[i].str[j + 1] == '?'
+			|| (is_quote(tokens[i].str[j + 1]) && quote == '\0')));
+}
+
+void	handle_expansion(t_tab *tokens, t_env *env, int i, char quote)
+{
+	expand_var(&tokens[i].str, env);
+	if (quote == '\0')
+		clear_space(tokens[i].str);
+}
